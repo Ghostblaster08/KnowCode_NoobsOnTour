765c139dd91d710b391ab62d015fb561b680cddc
Soham <141822047+XohamOG@users.noreply.github.com>
2025-01-25 03:54:44 +0530

    react scanner working

--- Green_Gauge/Green_Gauge/urls.py
+++ Green_Gauge/Green_Gauge/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for Green_Gauge project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,32 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('LandingPage.urls')),  # Landing page route
     path('api/scanner/', include('Scanner.urls')),  # API route for image upload (scanner)
-    path('quiz/', include('Trivia_Quizzes.urls')),
-    path('api/users/', include('users.urls')),
-]
-
-"""
-URL configuration for Green_Gauge project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('LandingPage.urls')),  
-    path('quiz/', include('Trivia_Quizzes.urls')),
-    path('api/users/', include('users.urls')),
+    path('quiz/', include('Trivia_Quizzes.urls')),  # Trivia quizzes route
+    path('api/users/', include('users.urls')),  # Users API route
 ]
--- Green_Gauge/Scanner/urls.py
+++ Green_Gauge/Scanner/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ImageUploadView  # Import the class-based view
 
 urlpatterns = [
-    path('scan/', ImageUploadView().as_view(), name='image-upload'),  # Use .as_view() to call the class-based view
+   path('scan/', ImageUploadView.as_view(), name='image-upload'),  # Use .as_view() to call the class-based view
 ]
