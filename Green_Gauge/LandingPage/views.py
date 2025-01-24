94328fe9d983e4d7edf609c2ef16177862c80aa3
Jai Desar <141903653+Ghostblaster08@users.noreply.github.com>
2025-01-24 17:04:20 +0530

    landingPage views

--- Green_Gauge/LandingPage/views.py
+++ Green_Gauge/LandingPage/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def home_dashboard(request):
+    return render(request, 'home/dashboard.html')
+
+def login_view(request):
+    return render(request,'login.html')
+
+def signup_view(request):
+    return render(request,'signup.html')
